3afb688ba8965de0f6b055e86a00ee251dcc5500
Ted Spence <tspence@lockstep.io>
2022-01-30 22:19:20 -0800

    Looks like rollup generates types now

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,27 @@
 // rollup.config.js
 import { createBasicConfig } from '@open-wc/building-rollup';
+import typescript from '@rollup/plugin-typescript';
+import dts from "rollup-plugin-dts";
 
 const baseConfig = createBasicConfig();
 
-export default {
+const config = [
+  {
     input: 'build/compiled/index.js',
     output: {
       file: 'lockstep-api.js',
-      format: 'cjs'
-    }
-  };
+      format: 'cjs',
+      sourcemap: true,
+    },
+    plugins: [typescript()]
+  }, {
+    input: 'build/compiled/LockstepApi.d.ts',
+    output: {
+      file: 'lockstep-api.d.ts',
+      format: 'es'
+    },
+    plugins: [dts()]
+  }
+];
+
+export default config;
