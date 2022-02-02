ad203344b7dd8731f41b0900d0df6e46dd6fca96
Ted Spence <tspence@lockstep.io>
2022-02-02 13:59:09 -0800

    Working on fixing export statements

--- rollup.config.js
+++ rollup.config.js
@@ -13,6 +13,7 @@ const config = [
       format: 'cjs',
       sourcemap: true,
     },
+    external: ['axios', 'os', 'url'],
     plugins: [typescript()]
   }, {
     input: 'build/compiled/LockstepApi.d.ts',
