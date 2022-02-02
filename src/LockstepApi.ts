95e9d3c679e29f2251628e8d889f0630254f7417
Ted Spence <tspence@lockstep.io>
2022-02-02 12:58:14 -0800

    Generate schema changes for week 5

--- src/LockstepApi.ts
+++ src/LockstepApi.ts
@@ -8,7 +8,7 @@
  *
  * @author     Ted Spence <tspence@lockstep.io>
  * @copyright  2021-2022 Lockstep, Inc.
- * @version    2022.5.7
+ * @version    2022.5.19
  * @link       https://github.com/Lockstep-Network/lockstep-sdk-typescript
  */
 
@@ -63,7 +63,7 @@ export class LockstepApi {
 
   // The URL of the environment we will use
   private readonly serverUrl: string;
-  private readonly version: string = "2022.5.7";
+  private readonly version: string = "2022.5.19";
   private bearerToken: string | null = null;
   private apiKey: string | null = null;
   private sdkName = "TypeScript";
--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@
  *
  * @author     Ted Spence <tspence@lockstep.io>
  * @copyright  2021-2022 Lockstep, Inc.
- * @version    2022.5.7
+ * @version    2022.5.19
  * @link       https://github.com/Lockstep-Network/lockstep-sdk-typescript
  */
 
