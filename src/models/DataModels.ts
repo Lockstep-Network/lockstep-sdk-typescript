ce6adf0d431fd14ecb3b47664ad2adda578a73a7
David Hric <dhric@lockstep.io>
2022-05-04 13:32:05 +0000

    Correction to datamodel

--- src/models/DataModels.ts
+++ src/models/DataModels.ts
@@ -2970,6 +2970,27 @@ export type CustomerSummaryModel = {
 };
 
 /**
+ * Represents the daily Payable outstanding report
+ */
+ export type DailyPayableOutstandingReportModel = {
+
+  /**
+   * Timeframe (quarter) the daily payable outstanding values are associated with
+   */
+  timeframe: string;
+
+  /**
+   * Number of invoices the average daily payable outstanding is calculated on
+   */
+  invoiceCount: number;
+
+  /**
+   * Time (in days) between an invoice was completed paid off and when the invoice was issued
+   */
+  dailyPayableOutstanding: number;
+};
+
+/**
  * Represents the daily sales outstanding report
  */
 export type DailySalesOutstandingReportModel = {
@@ -6589,6 +6610,80 @@ export type UserRoleModel = {
    * The ID of the user who last modified the user role
    */
   modifiedUserId: string;
+};
+
+/**
+ * Contains summarized data for a vendor
+ */
+ export type VendorSummaryModel = {
+
+  /**
+   * The GroupKey uniquely identifies a single Lockstep Platform account.  All records for this
+   * account will share the same GroupKey value.  GroupKey values cannot be changed once created.
+   *
+   * For more information, see [Accounts and GroupKeys](https://developer.lockstep.io/docs/accounts-and-groupkeys).
+   */
+  groupKey: string;
+
+  /**
+   * The unique ID of this Vendor
+   */
+  vendorId: string;
+
+  /**
+   * The name of this Vendor
+   */
+  vendorName: string | null;
+
+  /**
+   * The app enrollment ID this Vendor is associated with
+   */
+  appEnrollmentId: string | null;
+
+  /**
+   * The name of this Vendor's primary contact
+   */
+  primaryContactName: string | null;
+
+  /**
+   * This Vendor's primary contact id
+   */
+  primaryContactId: string | null;
+
+  /**
+   * The amount paid to this Vendor in the last 30 days
+   */
+  amountPaidLast30: number | null;
+
+  /**
+   * The outstanding advance pay balance with this Vendor
+   */
+  advancePayOutstanding: number | null;
+
+  /**
+   * The amount billed from this Vendor in the last 30 days
+   */
+  amountBilledLast30: number | null;
+
+  /**
+   * The outstanding balance with this Vendor
+   */
+  amountBilledOutstanding: number | null;
+
+  /**
+   * The number of open bills with this Vendor
+   */
+  openBillCount: number | null;
+
+  /**
+   * The number of bills paid to this Vendor in the last 30 days
+   */
+  paidBillCount: number | null;
+
+  /**
+   * The total count of open bills and those paid in the last 30 days
+   */
+  totalBillCount: number | null;
 };
 
 /**
